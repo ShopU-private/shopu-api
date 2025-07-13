f60ba30123f10e8c2322dd840461700b4b71a668
ayush <gdadvertising2020@gmail.com>
2025-07-13 16:32:41 +0530

    Deployment Error fix

--- src/main/java/com/shopu/ShopUApiApplication.java
+++ src/main/java/com/shopu/ShopUApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:AppSecrets.properties")
 public class ShopUApiApplication {
 
 	public static void main(String[] args) {
