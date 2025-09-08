89969cd0c98c2247f687499ad2050a548e7ee7a8
Ayush kumar <ayushkumar@Ayushs-MacBook-Air.local>
2025-09-08 16:04:49 +0530

    update web url in security config

--- src/main/java/com/shopu/authentication/SecurityConfig.java
+++ src/main/java/com/shopu/authentication/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         // CORS origins confirmation
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173" // React local
+                "https://shopu-admin-web.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
